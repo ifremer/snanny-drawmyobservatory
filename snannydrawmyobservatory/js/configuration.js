b5de8ea9c1faee62fc4b711ca0c7835aaba531e5
mcailleaux <mcailleaux@asi.fr>
2016-04-27 10:36:24 +0200

    ajout d'une base url en configuration pour préfixer la valeur sml:typeOf de l'export de type model

--- snannydrawmyobservatory/js/configuration.js
+++ snannydrawmyobservatory/js/configuration.js
@@ -1,3 +1,4 @@
 var birtserverLink ='http://isi.ifremer.fr/birt-viewer-4.3.2/frameset?__report=snanny/reports/new_report_3.rptdesign';
 var emsoYellowPage ='http://www.esonetyellowpages.com';
 var imageExternalHost = 'http://134.246.144.111:8888';
+var smlTypeOfUrl = 'http://www.ifremer.fr/';
